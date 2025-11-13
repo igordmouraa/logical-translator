62749a69587a6e248186d5ec5d293131f06093cd
igordmouraa <igordmoura_@hotmail.com>
2025-11-12 21:30:34 -0300

    feat: Create logo icon

---
+++ src/components/icons/logo-icon.tsx
@@ -0,0 +1,28 @@
+export function LogoIcon(props: React.SVGProps<SVGSVGElement>) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      {...props}
+    >
+      <path d="M12 2a4.5 4.5 0 0 0-4.5 4.5c0 1.2.4 2.3 1.2 3.2-1.3 1-2.2 2.7-2.2 4.6 0 2.8 2.2 5 5 5s5-2.2 5-5c0-1.9-.9-3.6-2.2-4.6.8-.9 1.2-2 1.2-3.2A4.5 4.5 0 0 0 12 2Z" />
+      <path d="M12 14v1" />
+      <path d="M12 19v2" />
+      <path d="M16 13h-1" />
+      <path d="M20 13h-2" />
+      <path d="M8 13H7" />
+      <path d="M4 13H2" />
+      <path d="m14.5 16.5-.8-.8" />
+      <path d="m18 20-.8-.8" />
+      <path d="m9.5 16.5.8-.8" />
+      <path d="m6 20 .8-.8" />
+    </svg>
+  );
+}
